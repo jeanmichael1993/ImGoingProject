f1625621fa6b75dcc5cab7b29bd1e681f872d41b
jeanmichael1993 <jeanmichael760@gmail.com>
2020-12-14 20:11:50 -0300

    User entity e Resource

---
+++ src/main/java/com/imGoingProject/imGoing/Resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.imGoingProject.imGoing.Resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imGoingProject.imGoing.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","Maria@gmail.com","1234","123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
