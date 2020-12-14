0eae894f027848293c037fbca0dec3dedd639640
jeanmichael1993 <jeanmichael760@gmail.com>
2020-12-14 19:02:40 -0300

    Projeto Criado

---
+++ src/main/java/com/imGoingProject/imGoing/ImGoingApplication.java
@@ -0,0 +1,13 @@
+package com.imGoingProject.imGoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImGoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImGoingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imGoingProject/imGoing/ImGoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imGoingProject.imGoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImGoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
