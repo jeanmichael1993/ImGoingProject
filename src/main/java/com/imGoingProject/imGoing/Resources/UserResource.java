2d0c710967e0e25602c803cd746bfbaf28049b52
jeanmichael1993 <jeanmichael760@gmail.com>
2020-12-14 22:34:39 -0300

    H2 database, test profile,JPA

--- src/main/java/com/imGoingProject/imGoing/Resources/UserResource.java
+++ src/main/java/com/imGoingProject/imGoing/Resources/UserResource.java
@@ -8,12 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.imGoingProject.imGoing.entities.User;
 
 @RestController
+//user é o caminho para a requisição
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	//metodo para pegar um dado
 	@GetMapping
+	//findall para pegar todos
 	public ResponseEntity<User> findAll(){
+		//criando um objeto com dados
 		User u = new User(1L,"Maria","Maria@gmail.com","1234","123");
+		//retornar o objeto
 		return ResponseEntity.ok().body(u);
 	}
 
