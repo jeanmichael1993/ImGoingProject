10e0d6d68bce8670252871852f1d88db9f4d10cc
jeanmichael1993 <jeanmichael760@gmail.com>
2020-12-15 10:11:39 -0300

    OrderStatus enum

--- src/main/java/com/imGoingProject/imGoing/Config/TestConfig.java
+++ src/main/java/com/imGoingProject/imGoing/Config/TestConfig.java
@@ -12,6 +12,7 @@ import com.imGoingProject.imGoing.Repositories.OrderRepository;
 import com.imGoingProject.imGoing.Repositories.UserRepository;
 import com.imGoingProject.imGoing.entities.Order;
 import com.imGoingProject.imGoing.entities.User;
+import com.imGoingProject.imGoing.entities.enums.OrderStatus;
 
 @Configuration
 @Profile("test")
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2,OrderStatus.WAITING_PAYMENT);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1,OrderStatus.WAITING_PAYMENT);
 		
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
